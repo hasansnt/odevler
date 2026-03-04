3017a2a3441aa5c1d8d58715fdd3388e7b0f4a5f
hasansnt <49034685+hasansnt@users.noreply.github.com>
2026-03-04 12:00:51 +0300

    Create odev2.c

---
+++ odev2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int sayi1, sayi2;
+    printf("Birinci sayıyı girin: ");
+    scanf("%d", &sayi1);
+    printf("İkinci sayiyi girin: ");
+    scanf("%d", &sayi2);
+    printf("Toplam: %d", sayi1+sayi2);
+
+    return 0;
+}
